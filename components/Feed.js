ff8216462cf2bf880665bc321e05dff870fbeced
royabhi647 <royabhi647@gmail.com>
2022-11-12 22:56:07 +0530

    version 1

--- components/Feed.js
+++ components/Feed.js
@@ -1,10 +1,29 @@
 import { Avatar } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Upload from "./Upload";
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { doc, onSnapshot } from "firebase/firestore";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth";
+import { db } from "../firebase";
 
 function Feed() {
+
+  const {user} = useContext(AuthContext) 
+
+  useEffect(()=>{
+    console.log(user.uid);
+    const unsub = onSnapshot(doc(db,"users",user.uid),(doc)=>{
+      console.log(doc.data());
+      // setUserData(doc.data())
+    })
+
+    return () =>{
+      unsub();
+    }
+  },[user])
+
   return (
     <div className="feed-container">
       {/* Navbar */}
